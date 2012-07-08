eb646e9f1180ad7240889f0f431049ed5bd5b8ca
Gareth Holmes <garhol@3dahoy.co.uk>
2012-07-08 16:17:32 +0100

    move templates, create venue views and template.

--- lib/apps/clubmates/urls.py
+++ lib/apps/clubmates/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('clubmates.lib.apps.clubmates.views',
     url(r'^$', 'index', name="default-view"),
-	url(r'^places/(?P<loc>.+?)/$', 'show_location', {}, "default-location"),
+    url(r'^places/$', 'index', name="redir"),
+    url(r'^places/(?P<loc>.+?)/$', 'show_location', {}, "default-location"),
+    url(r'^venue/(?P<loc>.+?)/$', 'show_venue', {}, "default-venue"),
 )
--- lib/apps/clubmates/views.py
+++ lib/apps/clubmates/views.py
@@ -3,9 +3,13 @@ from django.template import RequestContext
 from models import Location, Message
 
 def index(request):
-	context = {}
-	context['lumpy'] = "scrumpy"
-	return render_to_response('www/index.html', context, context_instance=RequestContext(request))
+    context = {}
+    try:
+        children = Location.objects.filter(parent=6).exclude(venue=True).order_by('name')
+        context['children'] = children
+    except:
+        pass
+    return render_to_response('www/index.html', context, context_instance=RequestContext(request))
 	
 def show_location(request, loc):
 	context = {}
@@ -40,5 +44,26 @@ def show_location(request, loc):
 		pass
     	
 	context['curr_location'] = loc
-	return render_to_response('www/places/index.html', context, context_instance=RequestContext(request))
-	
+	return render_to_response('www/place/index.html', context, context_instance=RequestContext(request))
+
+def show_venue(request, loc):
+    context = {}
+    try:
+        my_location = Location.objects.get(id=loc)
+        context['curr_location'] = my_location
+        context['parent'] = my_location.parent
+        context['lat'] = my_location.latitude
+        context['lon'] = my_location.longitude
+        context['zoom'] = 17
+    except:
+        context['curr_location'] = loc
+        context['new_location'] = True
+        
+    try:
+        siblings = Location.objects.filter(parent=my_location.parent).exclude(id=loc).order_by('name')
+        context['siblings'] = siblings
+    except:
+        pass
+    
+    return render_to_response('www/venue/index.html', context, context_instance=RequestContext(request))
+    
